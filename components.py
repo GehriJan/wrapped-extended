e4c2403c8c3a787bde0412f93a01c73252aa6e01
Jannis Gehring <jannis-gehring@t-online.de>
2024-12-26 16:01:08 +0100

    feat: first heatmap

---
+++ components.py
@@ -0,0 +1,61 @@
+import numpy as np
+import pandas as pd
+import abc
+import random
+import datetime
+from pyecharts import options as opts
+from pyecharts.charts import Calendar
+
+
+class DashboardComponent(abc.ABC):
+    def __init__(self):
+        pass
+
+    def render(self, df):
+        data = self.preprocess(df)
+        html_content = self.get_html_content(data)
+
+    @abc.abstractmethod
+    def preprocess(self, df: pd.DataFrame):
+        pass
+
+    @abc.abstractmethod
+    def get_html_content(self, data):
+        pass
+
+class Heatmap(DashboardComponent):
+    def preprocess(self, df):
+        return (
+            pd.to_datetime(df["ts"])
+            .dt.tz_convert("Europe/Berlin")
+            .dt.floor("D")
+            [(df["ts"] >= "2024-01-01") & ("2025-01-01" > df["ts"])]
+            .dt.date.value_counts()
+            .sort_index()
+        )
+    def get_html_content(self, data):
+        calendar = (
+            Calendar()
+            .add(
+                "Heatmap",
+                list(data.items()),
+                calendar_opts=opts.CalendarOpts(
+                    range_=[str(data.index.min()), str(data.index.max())],
+                    daylabel_opts=opts.CalendarDayLabelOpts(name_map="en"),
+                ),
+            )
+            .set_global_opts(
+                title_opts=opts.TitleOpts(title="Spotify Plays Heatmap"),
+                visualmap_opts=opts.VisualMapOpts(
+                    max_=int(np.quantile(data.values, 0.8)),
+                    min_=0,
+                    orient="horizontal",
+                    is_piecewise=False,
+                    pos_top="10px",
+                    pos_left="70%",
+                    range_color=["#ffffff", "#d9f2d9", "#65d46e"],
+                ),
+            )
+        )
+        html_content = calendar.render_embed()
+        return html_content
---
+++ main.py
@@ -0,0 +1,46 @@
+import random
+import datetime
+import json
+from pyecharts.charts import Calendar
+from pyecharts import options as opts
+import streamlit as st
+import pandas as pd
+import numpy as np
+import os
+import altair as alt
+import abc
+
+
+def read_spotify_data():
+    directory = (
+        "/Users/jannisgehring/VSCode/wrapped-extended/data/spotify_streaming_history"
+    )
+    dfs = []
+    for filename in os.listdir(directory):
+        file = os.path.join(directory, filename)
+        if not (os.path.isfile(file) and file.endswith(".json")):
+            continue
+        with open(file, "r") as f:
+            print(file)
+            json_data = json.load(f)
+            dfs.append(pd.DataFrame.from_dict(json_data))
+    df = pd.concat(dfs)
+    return df.reset_index(drop=True)
+
+
+def main():
+    st.set_page_config(
+        page_title="Spotify Data", page_icon=":chart_with_upwards_trend:", layout="wide"
+    )
+    st.title("Spotify Data")
+
+    df = read_spotify_data()
+    
+
+    st.components.v1.html(html_content, height=600)
+
+
+
+
+if __name__ == "__main__":
+    main()
